5f5998dac3f2bed1718bc90c569a6551c10019fb
Dejavu Moe <admin@dejavu.moe>
2022-12-16 21:52:42 +0800

    Modified run.sh

--- run.sh
+++ run.sh
@@ -17,4 +17,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/aistudio/libs/lib
 
 chmod +x gpg-fingerprint-filter-gpu
 
-./gpg-fingerprint-filter-gpu -a ed25519 -j 4096 -t 59000051 "x{16}|x{15}|x{14}|x{8}y{8}|x{4}y{12}" /root/paddlejob/workspace/output/gpg14.key
+./gpg-fingerprint-filter-gpu -a ed25519 -j 4096 -t 59654321 "x{16}|x{15}|x{14}|x{8}y{8}|x{4}y{12}" /root/paddlejob/workspace/output/output.pgp
